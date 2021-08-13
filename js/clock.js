78008461daec52cd66edb3b1a431da46d0ebb512
MiareJ <psdsd@naver.com>
2021-08-13 15:59:12 +0900

    add clock

---
+++ js/clock.js
@@ -0,0 +1,12 @@
+const clock = document.querySelector("#clock");
+
+function getClock() {
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+getClock(); // start clock function when the browser is opened
+setInterval(getClock, 1000); // repeat function every 1000ms
